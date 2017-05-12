d1ce9dfd50f1e3c862845451a9a7a52f93756733
yhl221 <1727538058@qq.com>
2017-05-12 20:39:11 +0800

    modify server

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const path = require('path');
 
-
+const app = new express();
 app.use(express.static('./public'));
+
+
+app.get('*', (req, res) => {
+    "use strict";
+    res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+});
+
+
 
 app.listen(3000, function () {
     console.log('Example app listening at port 3000：');
